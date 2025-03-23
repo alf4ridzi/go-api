a82693cedbacc3437169dcb513c770a053ccbedc
alf4ridzi <aalalfaridzi9@gmail.com>
2025-03-23 23:02:42 +0700

    add role model

--- models/user.model.go
+++ models/user.model.go
@@ -4,10 +4,18 @@ import (
 	"gorm.io/gorm"
 )
 
+type userRole string
+
+const (
+	ADMIN userRole = "admin"
+	USER  userRole = "user"
+)
+
 type User struct {
 	gorm.Model
-	Username string  `gorm:"not null;unique" json:"username"`
-	Name     string  `gorm:"type:varchar(100);not null" json:"name"`
-	Email    *string `gorm:"unique" json:"email"`
-	Password string  `gorm:"not null" json:"-"`
+	Username string   `gorm:"not null;unique" json:"username"`
+	Name     string   `gorm:"type:varchar(100);not null" json:"name"`
+	Email    *string  `gorm:"unique;type:varchar(255)" json:"email"`
+	Role     userRole `gorm:"type:varchar(10);not null;default:'user'" json:"role"`
+	Password string   `gorm:"not null" json:"-"`
 }
